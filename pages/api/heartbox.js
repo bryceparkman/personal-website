06096cba2fb133635ad65f59481c135b57ffa012
Bryce Parkman <brycecparkman@gmail.com>
2020-08-27 11:39:53 -0400

    Made get request less data

--- pages/api/heartbox.js
+++ pages/api/heartbox.js
@@ -28,7 +28,7 @@ export default async (req, res) => {
         res.end();
     }
     else if(req.method === 'GET'){
-        res.status(200).json(state);
+        res.status(200).send('K=' + state["K"] + '&B=' + state["B"]);
         res.end();
     }
     else {
