446beb743d4cbf2b9c767da58a2b8ffcd3e310ef
Bryce Parkman <brycecparkman@gmail.com>
2020-09-16 15:49:29 -0400

    Added navbar and intro blurb

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
@@ -9,7 +10,15 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello World</p>
+        <p>Computer Science and Computational Mathematics Student</p>
+      </section>
+      <section className={utilStyles.lightText} style={{textAlign: 'center'}}>
+        <p>
+          Welcome to my website! I am a Computer Science and Mathematics major at the University of Massachusetts Amherst. 
+          I am interested in all things robotics and the intersection of computer science and math. 
+          Experienced in full stack web development, app development, and mathematical visualization. 
+          Check out my projects to see these skills in action.
+      </p>
       </section>
     </Layout>
   )
---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout'
+
+export default function Project() {
+    return (
+        <Layout>
+            <h1>My Projects</h1>
+        </Layout>
+    )
+  }
+  
