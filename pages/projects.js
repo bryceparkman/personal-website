acb8a4b0492b88a310366c259f0ac8ae8396cb04
Bryce Parkman <brycecparkman@gmail.com>
2020-09-17 00:26:46 -0400

    Added Construction warning

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,13 @@
 import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Project() {
     return (
         <Layout>
-            
+            <section className={utilStyles.headingMd} style={{marginTop: '20px'}}>
+                <i className="fa fa-exclamation-triangle fa-5x" style={{color: '#FFCC00'}}></i>
+                <p style={{marginTop: '30px'}}>This page is under construction. Come back later!</p>
+            </section>
         </Layout>
     )
-  }
-  
+}
