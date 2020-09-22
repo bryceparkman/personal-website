c65402c0c980d30f526c8e80feb0259ad13c6795
Bryce Parkman <brycecparkman@gmail.com>
2020-09-22 14:07:25 -0400

    Updated project title

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Project() {
     return (
         <Layout>
+            <Head>
+                <title>Projects</title>
+            </Head>
             <section className={utilStyles.headingMd} style={{marginTop: '20px'}}>
                 <i className="fa fa-exclamation-triangle fa-5x" style={{color: '#FFCC00'}}></i>
                 <p style={{marginTop: '30px'}}>This page is under construction. Come back later!</p>
