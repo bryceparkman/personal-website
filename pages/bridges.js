de7899c3792f6ada1188ba9823ddd2225e2ff853
Bryce Parkman <brycecparkman@gmail.com>
2020-09-26 21:29:22 -0400

    Added project pages

---
+++ pages/bridges.js
@@ -0,0 +1,13 @@
+export default function Bridges() {
+    return (
+        <Layout>
+            <Head>
+                <title>Student Bridges</title>
+            </Head>
+            <section className={utilStyles.headingMd} style={{marginTop: '20px'}}>
+                <i className="fa fa-exclamation-triangle fa-5x" style={{color: '#FFCC00'}}></i>
+                <p style={{marginTop: '30px'}}>This page is under construction. Come back later!</p>
+            </section>
+        </Layout>
+    )
+}
