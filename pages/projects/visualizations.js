0637f41c8f9c2104b6ff4e498cd8c0c7d165bec4
Bryce Parkman <brycecparkman@gmail.com>
2020-10-05 22:17:26 -0400

    Added styling and iframe

--- pages/projects/visualizations.js
+++ pages/projects/visualizations.js
@@ -1,6 +1,7 @@
 import Construction from '../../components/construction'
 import Head from 'next/head';
 import Layout from '../../components/layout'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Visualizations() {
     return (
@@ -9,6 +10,9 @@ export default function Visualizations() {
                 <title>Visualizations</title>
             </Head>
             <Construction/>
+            <div className={utilStyles.headingMd}>
+                <iframe src="https://editor.p5js.org/brycecparkman/embed/C5WPTqD4-" width={640} height={560}></iframe>
+            </div>
         </Layout>
     )
 }
