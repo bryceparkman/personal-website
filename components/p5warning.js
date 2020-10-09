a7ca59321b28ca6f498b3695056f65e6d5d56dc0
Bryce Parkman <brycecparkman@gmail.com>
2020-10-09 16:07:27 -0400

    Seperated warning

---
+++ components/p5warning.js
@@ -0,0 +1,7 @@
+export default function p5Warning() {
+    return (
+        <p>
+            If no sketch appears, make sure all p5.js domains are allowed on any adblockers or privacy tracking browser extensions.
+        </p>
+    );
+}
