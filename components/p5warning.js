e31dd48260fa16add4d59dd6ecc96b4933834be6
Bryce Parkman <brycecparkman@gmail.com>
2020-10-09 16:08:21 -0400

    Added textAlign to warning

--- components/p5warning.js
+++ components/p5warning.js
@@ -1,6 +1,6 @@
 export default function p5Warning() {
     return (
-        <p>
+        <p style={{textAlign: 'center'}}>
             If no sketch appears, make sure all p5.js domains are allowed on any adblockers or privacy tracking browser extensions.
         </p>
     );
