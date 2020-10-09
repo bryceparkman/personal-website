127c1aa6ca575eb262f55e289134f84628f4316c
Bryce Parkman <brycecparkman@gmail.com>
2020-10-09 15:41:12 -0400

    Added projects page

--- pages/projects/visualizations.js
+++ pages/projects/visualizations.js
@@ -1,18 +1,64 @@
 import Construction from '../../components/construction'
 import Head from 'next/head';
 import Layout from '../../components/layout'
+import styles from '../../components/layout.module.css'
 import utilStyles from '../../styles/utils.module.css'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Navigation from '../../components/navigation'
 
 export default function Visualizations() {
     return (
-        <Layout>
+        <div style={{padding: '1rem', margin: '0.5rem auto'}}>
             <Head>
                 <title>Bryce Parkman - Visualizations</title>
             </Head>
-            <Construction/>
-            <div className={utilStyles.headingMd}>
-                <iframe src="https://editor.p5js.org/brycecparkman/embed/C5WPTqD4-" width={640} height={580} frameBorder={0}></iframe>
-            </div>
-        </Layout>
+            <div className={styles.navbar}>
+            <Navigation />
+          </div>
+            <Construction />
+            <Container>
+                <Row>
+                    <Col lg={4}>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Falling, Changing</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col lg={4}>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Falling, Changing</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col lg={4}>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Falling, Changing</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
