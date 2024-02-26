f2bef41d0a83f7dc50d076ed2d214780fd71dc1a
bryceparkman <brycecparkman@gmail.com>
2024-02-25 21:25:13 -0500

    dovey

--- pages/index.js
+++ pages/index.js
@@ -10,31 +10,55 @@ import ProjectOverview from './overview.js'
 import Image from 'react-bootstrap/Image'
 import Card from 'react-bootstrap/Card'
 
-// import { useEffect, useMemo, useState } from "react";
-// import Particles, { initParticlesEngine } from "@tsparticles/react";
-// import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 export default function Home() {
-  // const [init, setInit] = useState(false);
+  const [init, setInit] = useState(false);
 
-  // useEffect(() => {
-  //   initParticlesEngine(async (engine) => {
-  //     //await loadAll(engine);
-  //     //await loadFull(engine);
-  //     await loadSlim(engine);
-  //     //await loadBasic(engine);
-  //   }).then(() => {
-  //     setInit(true);
-  //   });
-  // }, []);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-  // const particlesLoaded = (container) => {
-  //   console.log(container);
-  // };
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
-  // const options = useMemo(
-  //   () => ({ "fullScreen": false, "background": { "image": " linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)" }, "particles": { "number": { "value": 10, "density": { "enable": true, "value_area": 600 } }, "color": { "value": "#ffffff" }, "shape": { "type": "square", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 } }, "opacity": { "value": 0.25, "random": true, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 29, "random": true, "anim": { "enable": false, "speed": 2, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": false, "distance": 300, "color": "#ffffff", "opacity": 0, "width": 0 }, "move": { "enable": true, "speed": 0.5, "direction": "top", "straight": true, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": false, "mode": "repulse" }, "onclick": { "enable": false, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 800, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 790, "size": 79, "duration": 2, "opacity": 0.8, "speed": 3 }, "repulse": { "distance": 400, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true })
-  // )
+  const options = useMemo(
+    () => ({"fullScreen": false, 
+            "background": { "image": " linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)" },
+            "particles": {
+              "number": { "value": 50,"density": { "enable": true, "value_area": 600 } },
+              "color": { "value": "#ffffff" },
+              "shape": { "type": "square",
+              "stroke": { "width": 0, "color": "#000000" },
+              "polygon": { "nb_sides": 5 } },
+              "opacity": {
+                "value": 0.25,
+                "random": true,
+                "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false }
+              },
+              "size": { "value": 29, "random": true, "anim": { "enable": false, "speed": 2, "size_min": 0.1, "sync": false } },
+              "line_linked": { "enable": false, "distance": 300, "color": "#ffffff", "opacity": 0, "width": 0 },
+              "move": { "enable": true, "speed": 0.5, "direction": "top", "straight": true,
+              "out_mode": "out", "bounce": false,"attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } }
+            },
+            "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": false, "mode": "repulse" },
+            "onclick": { "enable": false, "mode": "push" }, "resize": true },
+            "modes": { "grab": { "distance": 800, "line_linked": { "opacity": 1 } },
+            "bubble": { "distance": 790, "size": 79, "duration": 2, "opacity": 0.8, "speed": 3 },
+            "repulse": { "distance": 400, "duration": 0.4 }, "push": { "particles_nb": 4 },
+            "remove": { "particles_nb": 2 } } },
+            "retina_detect": true })
+  )
 
   return (
     <Layout page='home'>
@@ -42,12 +66,12 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <div id={styles.indexPage}>
-        <Container className={styles.header} id='topAnchor'>
-          {/* <Particles
-            id="tsparticles"
+        <Particles
+            id={styles.tsParticles}
             particlesLoaded={particlesLoaded}
             options={options}
-          /> */}
+        />
+        <Container className={styles.header} id='topAnchor'>   
           <Row>
             <Col sm={8} className="ml-auto mr-auto">
               <div>
